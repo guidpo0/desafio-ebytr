0221b9d7f76dd41e20bde7660b5bc49a6129118c
Gui Pereira <guilhermepdo1508@gmail.com>
2021-11-04 18:16:56 -0300

    Feito server.js

---
+++ back-end/src/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const UsersRouter = require('./routes/UsersRouter');
+const TasksRouter = require('./routes/TasksRouter');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/users', UsersRouter);
+
+app.use('/router', TasksRouter);
+
+app.listen(PORT, () => { console.log(`Ouvindo a porta ${PORT}`); });
