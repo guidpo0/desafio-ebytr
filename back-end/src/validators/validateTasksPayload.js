89315284848cd4a873dbc3f9475cd55898bc14ec
Gui Pereira <guilhermepdo1508@gmail.com>
2021-11-04 18:56:39 -0300

    Feito validator para tasks payload

--- back-end/src/validators/validateTasksPayload.js
+++ back-end/src/validators/validateTasksPayload.js
@@ -1,10 +1,14 @@
 const Joi = require('joi');
 
-module.exports = (req, _res, next) => {
-  const { error } = Joi.object({
-    districtName: Joi.string().required(),
-    state: Joi.string().min(2).max(2).required(),
-  }).validate(req.body);
+const validateTasksPayload = (req, _res, next) => {
+  const { error } = Joi.array().items(
+    Joi.object({
+      description: Joi.string().required(),
+      state: Joi.string().valid('pendente', 'em andamento', 'pronto').required(),
+    }),
+  ).validate(req.body);
   if (error) return next(error);
   return next();
 };
+
+module.exports = validateTasksPayload;
